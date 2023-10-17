b6e139e74703c1d1813f27f76a6f39a611bcf909
Candela Ares <cande.ares15@gmail.com>
2023-10-17 16:33:36 -0300

    tarea

---
+++ M3U2 tarea/routes/conocenos.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.send('hola soy la sección conocenos')
+  res.render('conocenos')
+});
+
+module.exports = router;
