97ed9a9b9380a6781d4cb0a78ed60c70b38328bc
Candela Ares <cande.ares15@gmail.com>
2023-12-07 15:53:48 -0300

    tarea

--- SITIO TATUAJES M3U3/routes/admin/novedades.js
+++ SITIO TATUAJES M3U3/routes/admin/novedades.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get('/', function (req, res, next) {
+
+/*get novedades page*/
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
-        layout:'admin/layout',
-        usuario: req.session.nombre
+        layout: 'admin/layout',
+        usuario: req.session.nombre,
+        novedades
     });
-  });
+});
+
+/*eliminar novedades*/
+router.get('/eliminar/:id', async (req, res, next) => {
+    const id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+});
 
 
-  module.exports = router;
+module.exports = router;
